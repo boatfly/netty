dc2abcd2924abf072c892755e15e737b582c6630
songgf <song761212_160906@qq.com>
2020-02-29 23:31:12 +0800

    Protobuf

---
+++ doc/Netty-编解码器机制.md
@@ -0,0 +1,45 @@
+# Netty 编解码器机制
+数据在网络传输的都是二进制字节码数据，在发送数据的时候就需要编码，接收数据时就需要解码。
+- codec(编解码器)的组成：
+  - decoder 解码器
+    - 将字节码数据转换为业务数据
+  - encoder 编码器
+    - 将业务数据转换为字节码数据
+    
+## Netty本身提供的解码器
+- 底层使用java序列化
+- 无法跨语言
+- 序列化的体积过大，是二进制编码的5倍多
+- 序列化性能太低
+- 由此------引出-------》更好的序列化方案：Google的Protobuf
+
+## Protobuf
+以message的方式管理数据，跨平台、跨语言，适合做数据存储或者RPC数据交换格式。
+
+`http+json` --转变--> `tcp+protobuf`
+
+### Protobuf install in Mac
+```
+1. https://github.com/protocolbuffers/protobuf/releases
+2. 下载带all的压缩包，譬如protobuf-all-3.6.1.zip，解压
+3. cd 解压目录
+4. sudo ./configure --prefix=/usr/local/protobuf 
+5. sudo make
+6. sudo make install
+7. 添加环境变量：vi .bash_profile
+8. export PROTOBUF=/usr/local/protobuf
+   export PATH=$PROTOBUF/bin:$PATH
+9  source .bash_profile
+10.验证: protoc --version
+```
+### 使用
+```
+1.切换到.proto文件目录
+2.protoc -I=./ --java_out=./ ./Student.proto
+  -I 等价于 -proto_path：指定 .proto 文件所在的路径
+  --java_out：编译成 java 文件时，标明输出目标路径
+  ./Student.proto：指定需要编译的 .proto 文件
+```
+
+
+
---
+++ src/main/java/com/boatfly/codehub/netty/examples/netty/codec/protobuf/NettyServer.java
@@ -0,0 +1,49 @@
+package com.boatfly.codehub.netty.examples.netty.codec.protobuf;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.io.IOException;
+
+public class NettyServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        //创建BossGroup WorkerGroup
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);//默认线程数为cpu核数*2 NettyRuntime.availableProcessors() * 2)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            //设置服务器端的启动对象
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup, bossGroup)
+                    .channel(NioServerSocketChannel.class) //使用NioSocketChannel作为服务器端channel的实现
+                    .option(ChannelOption.SO_BACKLOG, 128)//设置线程队列等待个数
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)//设置保持活动连接状态
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        //给pipeline设置处理器
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new NettyServerHandler());
+                        }
+                    });//为workgroup的eventloop对应的管道设置处理器
+            System.out.println("server is ready!....");
+
+
+            //绑定一个端口，并且同步，
+            ChannelFuture future = serverBootstrap.bind(9997).sync();
+
+            //对关闭通道进行监听
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
